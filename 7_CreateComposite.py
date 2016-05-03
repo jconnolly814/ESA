284cb37dba96f0f3ba9fcd41287e5e9ae7a216a4
Jennifer Connolly <je.connolly@gmail.com>
2016-05-03 18:09:54 -0400

    Changed from merge to append

--- 7_CreateComposite.py
+++ 7_CreateComposite.py
@@ -3,11 +3,16 @@ import datetime
 
 import arcpy
 
+
+
+## TODO make updated for append so that the len list is equal to the len count of rows of comp
 masterlist = 'J:\Workspace\MasterLists\April2015Lists\CSV\MasterListESA_April2015_20151015_20151124.csv'
+refFC = 'J:\Workspace\ESA_Species\Range\NAD83\Amphibians.gdb\R_3849_poly_20150415_STD_NAD83'
+
 gdbRegions_dict = 'J:\Workspace\ESA_Species\ForCoOccur\Dict\gdbRegions_dict.csv'
 outFolderCompGDB = r'J:\Workspace\ESA_Species\ForCoOccur\Composites\GDB\April_16Composites'
 skipgroup = []
-date = '20160502'
+date = '20160503'
 
 compfield = ['EntityID', 'FileName', 'NAME', 'Name_sci', 'SPCODE', 'VPCode']
 while True:
@@ -21,6 +26,7 @@ while True:
             FileType = "R_"
             filetype = '_R_'
             infolder = r"J:\Workspace\ESA_Species\ForCoOccur\Range"
+            projectionpath = 'J:\Workspace\projections'
             break
         else:
             CritHabFolder = r"J:\Workspace\ESA_Species\ForCoOccur\CriticalHabitat"
@@ -28,6 +34,7 @@ while True:
             FileType = "CH_"
             filetype = '_CH_'
             infolder = r"J:\Workspace\ESA_Species\ForCoOccur\CriticalHabitat"
+            projectionpath = 'J:\Workspace\projections'
             break
 while True:
     user_input2 = raw_input('Are you running the Lower48? Yes or No ')
@@ -37,7 +44,7 @@ while True:
         if user_input2 == 'Yes':
             L48 = True
             outGDB = outFolderCompGDB + os.sep + 'L48_' + FileType + 'SpGroup_Composite.gdb'
-            regiontype ="_L48_"
+            regiontype = "_L48_"
             break
         else:
             L48 = False
@@ -47,6 +54,7 @@ while True:
 
 arcpy.env.overwriteOutput = True  # ## Change this to False if you don't want GDB to be overwritten
 arcpy.env.scratchWorkspace = ""
+boolbreak = False
 
 
 def CreateGDB(OutFolder, OutName, outpath):
@@ -65,62 +73,59 @@ def CreateDirectory(path_dir, outLocationCSV, OutFolderGDB):
         os.mkdir(OutFolderGDB)
         print "created directory {0}".format(OutFolderGDB)
 
-def createcomp (group,regionname,regiontype, regionsgdb, date, filetype,projection,outGDB):
-    print "\nWorking on {0} in {1} inGDB is {2}".format(group, regionname, regionsgdb)
+
+def createcomp(group, regionname, regiontype, regionsgdb, date, filetype, projection, outGDB):
     Comp_FileName = regionname + filetype + group + regiontype + projection + "_" + str(date)
     filepath = outGDB + os.sep + Comp_FileName
-    print filepath
-
-    if not arcpy.Exists(filepath):
-        print regionsgdb
-        arcpy.env.workspace = regionsgdb
-        FeatureType = "Polygon"
-        wildcard = str(regionname +"_"+FileType) + "*"
-        print wildcard
-        fcList = arcpy.ListFeatureClasses(wildcard, FeatureType)
-        print fcList
-        print len(fcList)
-        if len(fcList) == 0:
-            pass
-        else: # Setup field mappings
-            pathlist =[]
-            for v in fcList:
-                pathfc = regionsgdb + os.sep + v
-                pathlist.append(pathfc)
-            skipfields = ["OBJECTID", "FID", "Shape"]
-            infc = pathlist[0]
-
-            fields = arcpy.ListFields(infc)
-            fms = arcpy.FieldMappings()
-            fieldname = 'FileName'
-            fieldlen = 75
-            for field in fields:
-                if field.name in skipfields:
-                     pass
-                else:
-                    fm = arcpy.FieldMap()
-                    fm.addInputField(infc, field.name)
-                    if field.name == fieldname:
-                        newfield = fm.outputField
-                        newfield.length = fieldlen
-                        fm.outputField = newfield
-                    fms.addFieldMap(fm)
-
-
-            arcpy.env.workspace = outGDB
-            arcpy.env.overwriteOutput = True
-            arcpy.Merge_management(pathlist, filepath, fms)
-            print filepath
-
-            count = int(arcpy.GetCount_management(filepath).getOutput(0))
-
-            if count != len(fcList):
-                print "Check {0} for missing files {1} ".format(group, regionname)
-
-            loop = datetime.datetime.now()
-            print "Elapse time for loop {0}".format(loop - start_script)
+    if arcpy.Exists(filepath):
+        count = int(arcpy.GetCount_management(filepath).getOutput(0))
     else:
+        count = 0
+
+    arcpy.env.workspace = regionsgdb
+    FeatureType = "Polygon"
+    if regionname == 'Lower48':
+        wildcard = str("L48_" + FileType) + "*"
+    else:
+        wildcard = str(regionname + "_" + FileType) + "*"
+    fcList = arcpy.ListFeatureClasses(wildcard, FeatureType)
+
+    if len(fcList) == 0:
         pass
+    elif len(fcList) == count:
+        print "  \n  Working on group {0}...".format(group)
+        print "    Already completed {0}".format(Comp_FileName)
+    else:
+        print "  \n  Working on group {0}...".format(group)
+        pathlist = []
+        for v in fcList:
+            fc = regionsgdb + os.sep + v
+            pathlist.append(fc)
+        print '    Total files are {0}'.format(len(pathlist))
+        arcpy.env.workspace = outGDB
+        arcpy.env.overwriteOutput = True
+
+        ORGdsc = arcpy.Describe(fc)
+        ORGsr = ORGdsc.spatialReference
+        print '    Spatial projections is: {0}'.format(ORGsr.name)
+
+        arcpy.CreateFeatureclass_management(outGDB, Comp_FileName, "POLYGON", refFC, 'DISABLED', 'DISABLED', ORGsr)
+        count = int(arcpy.GetCount_management(filepath).getOutput(0))
+        print '    Created blank fc {0} with row count {1}'.format(Comp_FileName, count)
+        print '    Appending files for {0} ....'.format(group)
+        arcpy.Append_management(pathlist, filepath)
+        count = int(arcpy.GetCount_management(filepath).getOutput(0))
+        if count != len(fcList):
+            print "    Check {0} for missing files {1}, projection , {0}".format(group, regionname, ORGsr, count)
+            boolbreak = True
+            return boolbreak
+        else:
+            boolbreak = False
+            return boolbreak
+
+        loop = datetime.datetime.now()
+        print "Elapse time for loop {0}".format(loop - start_script)
+
 
 start_script = datetime.datetime.now()
 if not os.path.exists(outGDB):
@@ -139,41 +144,48 @@ inputFile.close()
 unq_grps = set(grouplist)
 alpha_group = sorted(unq_grps)
 
-for group in alpha_group:
-    pcreated = False
-    if group in skipgroup:
-        continue
-    #print group
-    groupfolder = infolder + os.sep + group + os.sep + "Regions"
-    if group == "Ferns and Allies":
-        group = "Ferns"
-    elif group == 'Conifers and Cycads':
-        group = 'Conifers'
-    elif group == 'Flowering Plants':
-        group = 'Plants'
-    with open(gdbRegions_dict, 'rU') as inputFile2:
-        for line in inputFile2:
-            line = line.split(',')
-            gdb = str(line[0])
-            gdb = gdb.strip("\n")
-            regionsplit = gdb.split("_")
-            regionname = regionsplit[0]
-            projection = regionsplit[1]
-            projection = projection.strip('.gdb')
+with open(gdbRegions_dict, 'rU') as inputFile2:
+    for line in inputFile2:
+        if boolbreak:
+            break
+        line = line.split(',')
+        gdb = str(line[0])
+        gdb = gdb.strip("\n")
+        prj = str(line[2])
+        prj = projectionpath + os.sep + prj
+        projection = str(line[1])
+        projection = projection.strip('.gdb')
+        regionsplit = gdb.split("_")
+        regionname = regionsplit[0]
+        if L48:
+            if regionname != "Lower48":
+                continue
+            print '\nWorking on Region {0}, {1}...'.format(regionname, projection)
+
+        else:
+            if regionname == "Lower48":
+                continue
+            print '\nWorking on Region {0},  {1}...'.format(regionname, projection)
+        for group in alpha_group:
+            if boolbreak:
+                break
+            if group in skipgroup:
+                continue
+
+            groupfolder = infolder + os.sep + group + os.sep + "Regions"
+            if group == "Ferns and Allies":
+                group = "Ferns"
+            elif group == 'Conifers and Cycads':
+                group = 'Conifers'
+            elif group == 'Flowering Plants':
+                group = 'Plants'
             regionsgdb = groupfolder + os.sep + gdb
             if not arcpy.Exists(regionsgdb):
                 continue
-            if L48:
 
-                if regionname != "Lower48Only":
-                    continue
-                else:
-                    createcomp(group,regionname,regiontype,regionsgdb,date,filetype,projection,outGDB)
-            else:
+            boolbreak = createcomp(group, regionname, regiontype, regionsgdb, date, filetype, projection, outGDB)
 
-                if regionname == "Lower48Only":
-                    continue
-                else:
-                    createcomp(group,regionname,regiontype,regionsgdb,date,filetype,projection,outGDB)
+if boolbreak:
+    print 'Check for missing file for previous output'
 end = datetime.datetime.now()
 print "Elapse time {0}".format(end - start_script)
