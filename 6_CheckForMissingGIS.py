e9de39c2e79bac94a7365a4b69d11637f11fe1cd
Jennifer Connolly <je.connolly@gmail.com>
2016-05-18 12:51:25 -0400

    FirstPush

--- 6_CheckForMissingGIS.py
+++ 6_CheckForMissingGIS.py
@@ -1,20 +1,26 @@
 import datetime
 import os
+
 import pandas as pd
 import arcpy
 
 masterlist = 'J:\Workspace\MasterLists\April2015Lists\CSV\MasterListESA_April2015_20151015_20151124.csv'
 # TODO take table and load as a list in script so that there doesn't need to be a standalone document
 
-skiplist = []  # species groups that were already run
+
+outcsv = r'J:\Workspace\ESA_Species\ForCoOccur\Composites\GDB\April_16Composites\WebApp\CH_CheckMissingGIS_20160518.csv'
 
 index_dict = {'EntityID': 0,
+              'ComName': 1,
+              'Group': 2,
+              'SciName': 3,
+              'Status_text': 4,
               'AK': 5,
               'AS': 6,
               'CNMI': 7,
               'GU': 8,
               'HI': 9,
-              'Howland_Baker_Jarvis': 10,
+              'Howland Baker Jarvis': 10,
               'Johnston': 11,
               'L48': 12,
               'Laysan': 13,
@@ -23,30 +29,32 @@ index_dict = {'EntityID': 0,
               'Nihoa': 16,
               'NorthwesternHI': 17,
               'PR': 18,
-              'Palmyra_Kingman': 19,
+              'Palmyra Kingman': 19,
               'VI': 20,
               'Wake': 21}
 
-GDB_Dict = {'AK': 'AK_Singlepart.gdb',
-            'AS': 'AS_Singlepart.gdb',
-            'CNMI': 'CNMI_Singlepart.gdb',
-            'GU': 'GU_Singlepart.gdb',
-            'HI': 'HI_Singlepart.gdb',
-            'Howland_Baker_Jarvis': 'Howland_Singlepart.gdb',
-            'Johnston': 'Johnston_Singlepart.gdb',
-            'L48': 'Lower48_Singlepart.gdb',
-            'Laysan': 'Laysan_Singlepart.gdb',
-            'Mona': 'Mona_Singlepart.gdb',
-            'Necker': 'Necker_Singlepart.gdb',
-            'Nihoa': 'Nihoa_Singlepart.gdb',
-            'NorthwesternHI': 'NorthwesternHI_Singlepart.gdb',
-            'PR': 'PR_Singlepart.gdb',
-            'Palmyra_Kingman': 'Palmyra_Singlepart.gdb',
-            'VI': 'VI_Singlepart.gdb',
-            'Wake': 'Wake_Singlepart.gdb'}
+Region_cross = {'AK': 'AK',
+                'AS': 'AS',
+                'CNMI': 'CNMI',
+                'GU': 'GU',
+                'HI': 'HI',
+                'Howland Baker Jarvis': 'Howland_Baker_Jarvis',
+                'Johnston': 'Johnston',
+                'L48': 'L48',
+                'Lower48': 'L48',
+                'PLower48': 'L48',
+                'Laysan': 'Laysan',
+                'Mona': 'Mona',
+                'Necker': 'Necker',
+                'Nihoa': 'Nihoa',
+                'NorthwesternHI': 'NorthwesternHI',
+                'PR': 'PR',
+                'Palmyra Kingman': 'Palmyra_Kingman',
+                'VI': 'VI',
+                'Wake': 'Wake'}
 
-listKeys = sorted(index_dict.keys())
-print listKeys
+# listKeys = sorted(index_dict.keys())
+# print listKeys
 
 PossAnswers = ['Yes', 'No']
 
@@ -60,25 +68,20 @@ while QAanswer:
     else:
         QAanswer = False
         if user_input == 'Yes':
-            infolder = 'J:\Workspace\ESA_Species\ForCoOccur\Range'
+            infolder = 'J:\Workspace\ESA_Species\ForCoOccur\Composites\GDB\April_16Composites\WebApp\R_WebApp_Composite.gdb'
             regionallocations = 'J:\Workspace\MasterOverlap\Panda\NAD83_Range_SpeciesRegions_all_20160421.xlsx'
-            print 'Running range files output will be located at {0}'.format(infolder)
-        else:
-            infolder = 'J:\Workspace\ESA_Species\ForCoOccur\CriticalHabitat'
-            print 'Running critical habitat files output will be located at {0}'.format(infolder)
-            regionallocations = 'J:\Workspace\MasterOverlap\Panda\NAD83_CH_SpeciesRegions_all_20160421.xlsx'
+            fields = ['EntityID', 'Region']
 
-gdblist = ["SingleNonLower48only.gdb", "SingleBoth.gdb", "Lower48Only.gdb"]
+        else:
+            infolder = 'J:\Workspace\ESA_Species\ForCoOccur\Composites\GDB\April_16Composites\WebApp\CH_WebApp_Composite.gdb'
+            regionallocations = 'J:\Workspace\MasterOverlap\Panda\NAD83_CH_SpeciesRegions_all_20160421.xlsx'
+            fields = ['EntityID', 'Region']
+
 
 
 def CreateDirectory(DBF_dir):
     if not os.path.exists(DBF_dir):
         os.mkdir(DBF_dir)
-
-
-def CreateGDB(OutFolder, OutName, outpath):
-    if not arcpy.Exists(outpath):
-        arcpy.CreateFileGDB_management(OutFolder, OutName, "CURRENT")
 
 
 def fcs_in_workspace(workspace):
@@ -102,9 +105,45 @@ rowindex = rowcount.values[0]
 print rowindex
 
 listheader = regionaldf.columns.values.tolist()
+print listheader
 colcount = len(listheader)
 print colcount
 outDF = pd.DataFrame(columns=listheader)
+
+col = 0
+row = 0
+
+outdict = {}
+entid_list =[]
+while row <= (rowindex - 1):
+    currentlist = []
+    entidnext = index_dict['EntityID']
+    entid = str(regionaldf.iloc[row, entidnext])
+    if entid not in entid_list:
+        entid_list.append(entid)
+    for vol in listheader:
+        colvalue = index_dict[str(vol)]
+        value = regionaldf.iloc[row, colvalue]
+        currentlist.append(value)
+    outdict[entid] = currentlist
+    row += 1
+
+GISdict ={}
+
+for fc in fcs_in_workspace(infolder):
+    loop_start = datetime.datetime.now()
+    print "Loop Started: {0}".format(loop_start)
+    with arcpy.da.SearchCursor(fc, fields) as cursor:
+        for row in cursor:
+            entid = str(row[0])
+            region = str(row[1])
+            regionalloc = outdict[entid]
+            regionlookup = Region_cross[region]
+            region_index = index_dict[regionlookup]
+            currentvalues = regionalloc[region_index]
+            del regionalloc[region_index]
+            regionalloc.insert(region_index, (str(currentvalues) + ', ' + 'GIS'))
+            GISdict[entid] = regionalloc
 
 col = 0
 row = 0
@@ -114,95 +153,56 @@ while row <= (rowindex - 1):
     currentlist = []
     entidnext = index_dict['EntityID']
     entid = str(regionaldf.iloc[row, entidnext])
-    for vol in listKeys:
-        colvalue = index_dict[vol]
-        # print 'Current index is {0} , {1}'.format(row,colvalue)
+    for vol in listheader:
+        colvalue = index_dict[str(vol)]
         value = regionaldf.iloc[row, colvalue]
         currentlist.append(value)
-    # print currentlist
     outdict[entid] = currentlist
     row += 1
 
-# print outdict
-grouplist = []
-with open(masterlist, 'rU') as inputFile:
-    header = next(inputFile)
-    for line in inputFile:
-        line = line.split(',')
-        entid = line[0]
-        group = line[1]
-        grouplist.append(group)
+GISent = GISdict.keys()
+outlist = []
+for value in entid_list:
+    orgvalue = outdict[value]
+    counter = 0
 
-unq_grps = set(grouplist)
-alpha_group = sorted(unq_grps)
-print alpha_group
+    if value in GISent:
+        checkvalues = GISdict[value]
 
-for group in alpha_group:
-    if group in skiplist:
-        continue
-    print "\nWorking on {0}".format(group)
-    resultfolder = infolder + os.sep + group
-    regionsDIR = resultfolder + os.sep + "Regions"
-    arcpy.env.workspace = regionsDIR
-    workspaces = arcpy.ListWorkspaces("*", "FileGDB")
-    # for each group count the number of species in each region and generate a dict Birds: ['number in regions A',
-    # 'number in region b' etc. Then for each group loop through workspace count the number of files in gdb and see if
-    # it matches
+        while counter < colcount:
 
-
-
-        NAD83DIR = regionsDIR + os.sep + 'NAD83'
-        CreateDirectory(NAD83DIR)
-        gdb = GDB_Dict[regions]
-        outpathgdb = NAD83DIR + os.sep + gdb
-
-
-        for value in gdblist:
-            if value == "SingleBoth.gdb" and regions == "Lower48":
-                outpathgdb = NAD83DIR + os.sep + 'PLower48_Singlepart.gdb'
-                region = 'PLower48'
-
-
-            inGDB = resultfolder + os.sep + value
-            for fc in fcs_in_workspace(inGDB):
-                infc = inGDB + os.sep + fc
-                entid = fc.split("_")
-                entid = str(entid[1])
-                regionindex = listKeys.index(regions)
-                speinfo = outdict[entid]
-                # print speinfo
-                regionval = str(speinfo[regionindex])
-                # print regionval
-                if regionval != 'nan':
-                    outfc = outpathgdb + os.sep + region + "_" + str(fc)
-                    if not arcpy.Exists(outfc):
-                        if not arcpy.Exists(outpathgdb):
-                            CreateGDB(NAD83DIR, gdb, outpathgdb)
-                        arcpy.CopyFeatures_management(infc, outfc)
-                        print "Copied file {0} in region {1}".format(outfc, region)
-                    else:
-                        continue
+            GIS = str(checkvalues[counter])
+            old = str(orgvalue[counter])
+            if GIS == old:
+                pass
+            elif GIS == 'Yes, GIS' and old == 'Yes':
+                pass
+            elif GIS == 'nan, GIS' and old == 'nan':
+                pass
                 else:
-                    continue
+                    del checkvalues[counter]
+                    checkvalues.insert(counter, ('Error'))
+            counter += 1
+        outlist.append(checkvalues)
+    else:
+        counter = 5  ## Hard code to start it at the first colem that is a range
+
+        while counter < colcount:
+            old = str(orgvalue[counter])
+            if old == 'Yes':
+                del orgvalue[counter]
+                orgvalue.insert(counter, ('Yes, None'))
+            elif old == 'nan':
+                pass
+            else:
+                print 'Error'
+                orgvalue.insert(counter, ('Error'))
+            counter += 1
+        outlist.append(orgvalue)
+
+outDF = pd.DataFrame(outlist, columns=listheader)
+
+# print outDF
+outDF.to_csv(outcsv)
 
 print "Script completed in: {0}".format(datetime.datetime.now() - start_script)
-
-
-with open(masterlist, 'rU') as inputFile:
-    header = next(inputFile)
-    for line in inputFile:
-        line = line.split(',')
-        group = line[1]
-        grouplist.append(group)
-inputFile.close()
-
-unq_grps = set(grouplist)
-alpha_group = sorted(unq_grps)
-print alpha_group
-
-for group in alpha_group:
-    if group in skipgroup:
-        continue
-    groupfolder = inFolder + os.sep + group + os.sep + "Regions"
-arcpy.env.workspace = "C:/Data"
-workspaces = arcpy.ListWorkspaces("*", "FileGDB")
