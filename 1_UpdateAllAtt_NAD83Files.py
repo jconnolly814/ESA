f78033772a8b4464671c15d8b3ac8b47655f2332
Jennifer Connolly <je.connolly@gmail.com>
2016-06-15 14:18:50 -0400

    recoded to pandas

--- 1_UpdateAllAtt_NAD83Files.py
+++ 1_UpdateAllAtt_NAD83Files.py
@@ -2,19 +2,32 @@ import os
 import datetime
 
 import arcpy
+import pandas as pd
 
-masterlist = 'J:\Workspace\MasterLists\CSV\MasterListESA_April2015_20151015_20151124.csv'
-infolder = 'J:\Workspace\ESA_Species\Range\NAD83'  # folder of GDB
+masterlist = 'J:\Workspace\MasterLists\April2015Lists\CSV\MasterListESA_April2015_20151015_20151124.csv'
+infolder = 'J:\Workspace\ESA_Species\Range\NAD83\RawAquatics\Catch_notdissolved\Crustaceans_catchment.gdb'  # folder of GDB
 
 # species groups to skip
 skiplist = []
 
-ColIndexDict = dict(comname=7, sciname=8, spcode=26, vipcode=27, entid=0, group=1)
-final_fields = ['NAME', 'Name_sci', 'SPCode', 'VIPCode', 'FileName', 'EntityID']
-final_fieldsindex = dict(NAME=0, Name_sci=3, SPCode=4, VIPCode=5, EntityID=1)  # pos 2 is group
+final_fields = ['FileName', 'EntityID', 'NAME', 'Name_sci', 'SPCode', 'VIPCode', 'Status']  # in order to be added
+colorderDict = dict(EntityID='entityid', NAME='comname', Name_sci='sciname', SPCode='spcode', VIPCode='vipcode',
+                    Status='status')  # link the var used to store species info from table to a col in fc
+final_fieldsindex = dict(NAME=2, Name_sci=3, SPCode=4, VIPCode=5, EntityID=1, Status=6,
+                         FileName=0)  # used to check if FC has field in the correct order
 
+CheckForChanges_col = ['entityid', 'spcode', 'vipcode', 'sciname', 'comname', 'group',
+                       'status']  # col that will be looked up in master
+
+oldColindex = {'entityid': 0,
+               'spcode': 26,
+               'vipcode': 27,
+               'sciname': 8,
+               'comname': 7,
+               'group': 1,
+               'status': 11}
 ########Static variables
-reqindex = {'entid': 'Q1', 'group': 'Q2'}
+
 entid_indexfilenm = 1
 inputlist = ['Yes', 'No']
 speciestoQA = '1'
@@ -36,6 +49,51 @@ print "Script started at {0}".format(start_script)
 
 
 # Functions
+def CreatDicts(list_vars):
+    for v in list_vars:
+        dictname = "{0}".format(v)
+        globals()[dictname] = {}
+
+
+def list_dicts():
+    # global spcode, vipcode, sciname, comname, invname, pop_abbrev, pop_desc, family, status, status_text, lead_agency, lead_region, country, listing_date, dps, refuge_occurrence
+    listitems = []
+    listdicts = []
+    for name in globals():
+        if not name.startswith('__'):
+            listitems.append(name)
+    for value in listitems:
+        if type((globals()[value])) is dict:
+            listdicts.append(value)
+    return listdicts
+
+
+def LoadDatainDicts(incsv, colIndex, dict_list):
+    df = pd.read_csv(incsv)
+    group_df = df['Group']
+    unq_groups = group_df.drop_duplicates()
+
+    header = list(df.columns.values)
+
+    entidindex = colIndex["entityid"]
+
+    rowcount = df.count(axis=0, level=None, numeric_only=False)
+    rowindex = rowcount.values[0]
+    colindex = len(header) - 1  # to make base 0
+
+    row = 0
+    while row < (rowindex):
+        entid = str(df.iloc[row, entidindex])
+        # print "Working on species {0}, row {1}".format(entid, row)
+        for dictionary in dict_list:
+            col = colIndex[str(dictionary)]  ## assuming an index row from pandas export
+            value = df.iloc[row, col]
+            ((globals()[dictionary][entid])) = str(value)
+
+        row += 1
+    return group_df
+
+
 def fcs_in_workspace(workspace):
     arcpy.env.workspace = workspace
     for fc in arcpy.ListFeatureClasses():
@@ -43,16 +101,6 @@ def fcs_in_workspace(workspace):
     for ws in arcpy.ListWorkspaces():
         for fc in fcs_in_workspace(ws):
             yield fc
-
-
-def askuser(col, q, listQtype):
-    if q == 'Q1':
-        user_input = raw_input('What is the column index for the EntityID (base 0): ')
-        return user_input
-    else:
-        print listQtype
-        user_input = raw_input('What is the column index for the {0}(base 0): '.format(col))
-        return user_input
 
 
 def output_update(fc, value):
@@ -64,12 +112,11 @@ def CreateGDB(OutFolder, OutName, outpath):
         arcpy.CreateFileGDB_management(OutFolder, OutName, "CURRENT")
 
 
-## may be able to call dissolve field in update and so that is does  not need to re-load the fc list
-# TODO update files so that character limits in the attribute table doesn't block data update
-def updateFilesloop(inGDB, final_fields, speinfodict, DissolveFiles):
+def CheckFieldOrder(inGDB, final_fieldsindex, DissolveField):
     group_gdb = inGDB
     arcpy.env.workspace = group_gdb
     fclist = arcpy.ListFeatureClasses()
+    checkorder = {}
     for fc in fclist:
         print fc
         result = arcpy.GetCount_management(fc)
@@ -78,85 +125,71 @@ def updateFilesloop(inGDB, final_fields, speinfodict, DissolveFiles):
             DissolveFiles = True
         entid = fc.split('_')
         entid = str(entid[entid_indexfilenm])
+        order_correct = True
         fclist_field = [f.name for f in arcpy.ListFields(fc) if not f.required]
-        for field in final_fields:
-            if field not in fclist_field:
+        for field in fclist_field:
+            i = fclist_field.index(field)
+            checkorder[field] = i
+            j = final_fieldsindex[field]
+            if i != j:
+                order_correct = False
+            else:
+                continue
+        if not order_correct:
+            for field in fclist_field:
+                arcpy.DeleteField_management(fc, field)
+            for field in final_fields:
                 arcpy.AddField_management(fc, field, "TEXT")
                 print "added field {1} for {0}".format(fc, field)
-                with arcpy.da.UpdateCursor(fc, field) as cursor:
-                    if field == 'FileName':
-                        for row in cursor:
-                            fcname = fc.strip("_STD_NAD83")
-                            # print fcname
-                            value = str(fcname)
+
+        fclist_field = [f.name for f in arcpy.ListFields(fc) if not f.required]
+
+        for field in fclist_field:
+            print field
+            with arcpy.da.UpdateCursor(fc, field) as cursor:
+                if field == 'FileName':
+                    for row in cursor:
+                        current = row[0]
+                        if current is None:
+                            value = str(fc)
                             row[0] = value
                             cursor.updateRow(row)
                             output_update(fc, field)
-                    if field == 'EntityID':
-                        for row in cursor:
+                        else:
+                            continue
+                    continue
+                if field == 'EntityID':
+                    for row in cursor:
+                        current = row[0]
+                        if current != entid:
                             value = entid
                             row[0] = value
                             cursor.updateRow(row)
                             output_update(fc, field)
-                    else:
-                        for row in cursor:
-                            indexfield = final_fieldsindex[field]
-                            listspecies = speinfodict[entid]
-                            value = listspecies[indexfield]
+                        else:
+                            continue
+                else:
+                    val_dict = colorderDict[field]
+                    value = ((globals()[val_dict][entid]))
+                    for row in cursor:
+                        current = row[0]
+                        if current != value:
                             row[0] = value
                             cursor.updateRow(row)
                             output_update(fc, field)
-            else:
-                with arcpy.da.UpdateCursor(fc, field) as cursor:
-                    if field == 'FileName':
-                        for row in cursor:
-                            current = row[0]
-                            fcnamelist = fc.split("_")
-                            try:
-                                fcname = fcnamelist[0] + "_" + fcnamelist[1] + "_" + fcnamelist[2] + "_" + fcnamelist[3]
-                            except:
-                                fcname = fc
-                            if current == fcname:
-                                continue
-                            else:
-                                try:
-                                    row[0] = fcname
-                                    cursor.updateRow(row)
-                                    output_update(fc, field)
-                                except:
-                                    continue
-                    if field == 'EntityID':
-                        value = entid
-                        for row in cursor:
-                            current = row[0]
-                            if current == value:
-                                continue
-                            else:
-                                row[0] = value
-                                cursor.updateRow(row)
-                                output_update(fc, field)
-                    else:
-                        for row in cursor:
-                            current = row[0]
-                            indexfield = final_fieldsindex[field]
-                            listspecies = speinfodict[entid]
-                            value = listspecies[indexfield]
-                            if current == value:
-                                continue
-                            else:
-                                # noinspection PyBroadException
-                                try:
-                                    row[0] = value
-                                    cursor.updateRow(row)
-                                    output_update(fc, field)
-                                except:
-                                    continue
+                        else:
+                            continue
+
         for field in fclist_field:
             if field not in final_fields:
                 arcpy.DeleteField_management(fc, field)
             else:
                 continue
     return DissolveFiles
+
+
+## may be able to call dissolve field in update and so that is does  not need to re-load the fc list
+# TODO update files so that character limits in the attribute table doesn't block data update
 
 
 def dissolveloop(inGDB, final_fields):
@@ -181,85 +214,31 @@ def dissolveloop(inGDB, final_fields):
                 continue
 
 
-def LoadSpeciesinfo_frommaster(ColIndexDict, reqindex, masterlist):
-    grouplist = []
-    speciesinfo_dict = {}
-    listKeys = ColIndexDict.keys()
-    listKeys.sort()
-    reqlistkeys = sorted(reqindex.keys())
-    q1 = False
-    q2 = False
-    for val in reqlistkeys:
-        if val in listKeys:
+CreatDicts(CheckForChanges_col)
+allDicts = list_dicts()
+group_df = LoadDatainDicts(masterlist, oldColindex, CheckForChanges_col)
+
+if singleGDB:
+    inGDB = infolder
+    DissolveFiles = CheckFieldOrder(inGDB, final_fieldsindex, DissolveFiles)
+    if DissolveFiles:
+        dissolveloop(inGDB, final_fields)
+    DissolveFiles = False
+else:
+    alpha_group = group_df['Group'].values.tolist
+    for group in alpha_group:
+        start_loop = datetime.datetime.now()
+        if group in skiplist:
             continue
-        else:
-            question = reqindex[val]
-            vars()[question] = True
-            vars()[val] = askuser(val, question, listKeys)
-            if q1:
-                ColIndexDict['entid'] = vars()[val]
-            if q2:
-                ColIndexDict['group'] = vars()[val]
+        print "\nWorking on {0}".format(group)
+        inGDB = infolder + os.sep + str(group) + '.gdb'
+        DissolveFiles = CheckFieldOrder(inGDB, final_fieldsindex, DissolveFiles)
+        if DissolveFiles:
+            dissolveloop(inGDB, final_fields)
+        DissolveFiles = False
 
-    listKeys = ColIndexDict.keys()
-    listKeys.sort()
-    with open(masterlist, 'rU') as inputFile:
-        header = next(inputFile)
-        for line in inputFile:
-            speciesinfo = []
-            line = line.split(',')
-            entid = line[int(ColIndexDict['entid'])]
-            group = line[int(ColIndexDict['group'])]
-            if group not in grouplist:
-                grouplist.append(group)
-            for v in listKeys:
-                vars()[v] = line[int(ColIndexDict[v])]
-                speciesinfo.append(vars()[v])
-            speciesinfo_dict[entid] = speciesinfo
-    inputFile.close()
-    alpha = sorted(grouplist)
-    # print alpha_group
-    return listKeys, speciesinfo_dict, alpha
-
-
-listKeys, speciesinfo_dict, alpha_group = LoadSpeciesinfo_frommaster(ColIndexDict, reqindex, masterlist)
-
-print listKeys
-print speciesinfo_dict[speciestoQA]
-
-while not updatefiles:
-    user_inputupdated = raw_input('Please confirm that the species information is in the correct order: Yes or No: ')
-    if user_inputupdated not in inputlist:
-        print 'This is not a valid answer'
-    elif user_inputupdated == 'Yes':
-        updatefiles = True
-        print 'Files will be updated now'
-        if singleGDB:
-            inGDB = infolder
-            DissolveFiles = updateFilesloop(inGDB, final_fields, speciesinfo_dict, DissolveFiles)
-            if DissolveFiles:
-                dissolveloop(inGDB, final_fields)
-            DissolveFiles = False
-
-        else:
-            for group in alpha_group:
-                start_loop = datetime.datetime.now()
-                if group in skiplist:
-                    continue
-                print "\nWorking on {0}".format(group)
-                inGDB = infolder + os.sep + str(group) + '.gdb'
-                DissolveFiles = updateFilesloop(inGDB, final_fields, speciesinfo_dict, DissolveFiles)
-                if DissolveFiles:
-                    dissolveloop(inGDB, final_fields)
-                DissolveFiles = False
-
-                endloop = datetime.datetime.now()
-                print "Elapse time {0}".format(endloop - start_script)
-
-    else:
-        print 'Files will not be updated check the input indexes'
-        updatefiles = True
-        break
+        endloop = datetime.datetime.now()
+        print "Elapse time {0}".format(endloop - start_script)
 
 end = datetime.datetime.now()
 print "Elapse time {0}".format(end - start_script)
